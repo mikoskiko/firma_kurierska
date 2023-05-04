364e4c397b5d863e59767ea6b36e6cb45f321fff
Kuba <Kuba@DESKTOP-FGR0SFK>
2023-05-04 15:54:03 +0200

    Route creation of ant colony optimization algorithm

--- punkty.py
+++ punkty.py
@@ -1,3 +1,5 @@
+from this import s
+from xmlrpc.client import MAXINT
 import numpy as np
 import matplotlib.pyplot as plt
 import random
@@ -60,3 +62,54 @@ with open('distance.csv', 'w', newline='') as file:
 
 plt.plot(x,y,'ro')
 plt.show()
+
+#ants
+vehicleCount = 10
+maxWorkTime = 8
+maxCapacity = 8
+maxIterations = 100000
+iterationCount = 0
+candidateListSize = int(10 * number_of_points / 100)
+isVisited = np.full(number_of_points,False)
+isVisited[0] = True
+capacity = np.zeros(number_of_points)
+workTime = np.zeros(number_of_points)
+routes = np.zeros((vehicleCount, number_of_points))
+routeLength = np.zeros(vehicleCount)
+pheromones = np.ones((number_of_points, number_of_points))
+while(not any(isVisited) or iterationCount >= maxIterations):
+    for i in range(vehicleCount):
+        if(capacity[i] > maxCapacity or workTime > maxWorkTime):
+            for j in range(routeLength[i]):
+                isVisited[routes[i][j]] = False
+                routes[i][j] = 0
+            routeLength[i] = 0
+        if(routeLength[i]==0):
+            startPoint = random.randint(1,number_of_points)
+            isVisited[startPoint] = True
+            routeLength[i] = 1
+            routes[i][0] = startPoint
+        else:
+            startPoint = routes[i][routeLength[i]-1]
+
+        candidateList = np.argpartition(distance[startPoint], candidateListSize+1)[1:candidateListSize+1]
+        for j, candidate in enumerate(candidateList):
+            if (isVisited[candidate] == True):
+                candidateList.delete(j)
+        if(candidateList.size == 0):
+            propabilty = np.zeros(number_of_points)
+            for j in range(number_of_points):
+                if(isVisited[j] == True):
+                    propabilty[j] = MAXINT
+                else: 
+                    propabilty[j] = pheromones[startPoint][candidate] * 1 / distance[startPoint][candidate] * (distance[startPoint][0]+distance[0][candidate]-distance[startPoint][candidate])
+            nextPoint = propabilty.argmax(propabilty)
+        else:
+            propabilty = np.zeros(candidateList.size)
+            for j, candidate in enumerate(candidateList):
+                propabilty[j] = pheromones[startPoint][candidate] * 1 / distance[startPoint][candidate] * (distance[startPoint][0]+distance[0][candidate]-distance[startPoint][candidate]) 
+            nextPoint = random.choices(candidateList, propabilty, k=1)
+        routeLength[i]+=1
+        routes[i][routeLength] = nextPoint
+
+iterationCount += 1
